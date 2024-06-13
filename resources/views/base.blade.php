861d4b4f3d36aaf6ab1e47eff8f4eb870263f4c1
hamzalamin <hamzalamin80@gmail.com>
2024-06-13 09:34:56 +0200

    last commit

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,36 +1,61 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') | {{config('app.name')}}</title>
+    <link rel="stylesheet" href="/style.css">
+       
+    <link href="//fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style-starter.css">
 </head>
+
 <body>
-    <header class="main_menu home_menu">
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-                <a class="navbar-brand" href="{{ route('homePage') }}" ><img class="" width="50px" src="/storage/restaurant-logos-png-png_25706.jpg" alt="#"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+    <!-- header -->
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand active" href="{{ route('homePage') }}"><span>L</span>aundries<i class="fas fa-hands-wash"></i></a>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
                 </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ route('show') }}">Dashboard</a>
-           </li>
-           
-           <li class="nav-item">
-                <a class="nav-link" href="{{ route('add') }}">Add reccete</a>
-            </li>
-        </ul>
-            </div>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ route('add') }}">Add Recette</a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('show') }}">Dashboard</a>
+                        </li>
+                       
+                    </ul>
+                    
+                </div>
+                <!-- toggle switch for light and dark theme -->
+                <div class="cont-ser-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox">
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
+                        </div>
+                    </nav>
+                </div>
+                <!-- //toggle switch for light and dark theme -->
+            </nav>
         </div>
-    </nav>
     </header>
-    
     <div class="container">
         @if ($errors->any())
         
@@ -47,6 +72,84 @@
         @endif
         @yield('content')
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("movetop").style.display = "block";
+            } else {
+                document.getElementById("movetop").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
+
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+ 
+    <script src="assets/js/theme-change.js"></script>
+   
+    <script>
+        $(window).on("scroll", function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 80) {
+                $("#site-header").addClass("nav-fixed");
+            } else {
+                $("#site-header").removeClass("nav-fixed");
+            }
+        });
+
+        //Main navigation Active Class Add Remove
+        $(".navbar-toggler").on("click", function () {
+            $("header").toggleClass("active");
+        });
+        $(document).on("ready", function () {
+            if ($(window).width() > 991) {
+                $("header").removeClass("active");
+            }
+            $(window).on("resize", function () {
+                if ($(window).width() > 991) {
+                    $("header").removeClass("active");
+                }
+            });
+        });
+    </script>
+    
+    <script>
+        $(function () {
+            $('.navbar-toggler').click(function () {
+                $('body').toggleClass('noscroll');
+            })
+        });
+    </script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
+
+  
+
+
+
+
+
+
+
+
+   
+    <button onclick="topFunction()" id="movetop" title="Go to top">
+        <span class="fas fa-level-up-alt" aria-hidden="true"></span>
+    </button>
+  
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,25 +2,23 @@
 @section('title', 'recettes')
 @section('content')
 
-<section class="banner_part py-5 mb-5">
-    <div class="container mb-5">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="banner_text">
-                    <div class="banner_text_iner">
-                        <h5 class="mb-4 custom-heading">Our Recettes</h5>
-                        <h1 class="mb-4">Deliciousness jumping into the mouth</h1>
-                        <p class="mb-4">Together creeping heaven upon third dominion be upon won't darkness rule land behold it created good saw after she'd Our set living. Signs midst dominion creepeth morning</p>
-                    </div>
+<section class="banner-19">
+    <div class="banner-layer">
+            <img id="myVideo" src="assets/images/Capture6_PNG.png" alt="#">
+        <div class="main-content-top">
+            <div class="container">
+                <div class="main-content">
+                    <h4>Laundry & Our Recettes</h4>
+                    <p class="mt-3">Irferendis repudandae fugia rchitecto beatae rederit svitae recusa ndae
+                        debitifacere uiimi placeat maxienui </p>
+                    <a href="about.html" class="btn btn-style mt-md-5 mt-4">Read More</a>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <img src="storage/electro-maroc-2-65b3f59c5a3bc974840308.webp" alt="" class="img-fluid w-100 rounded-3">
             </div>
         </div>
     </div>
 </section>
-<section class="mx-1 mb-5">
+
+{{-- <section   class="mx-1 mb-5 fcpol"> --}}
 {{-- 
     <div class="container mb-5 mt-5">
         <div class="row justify-content-center align-items-center">
@@ -33,7 +31,124 @@
           </div>
         </div>
       </div> --}}
+
+
+          <section class="w3l-bottom-grids-6 py-5">
+        <div class="container pt-lg-5 pt-md-4 pt-2">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-6">
+                    <div class="area-box">
+                        <i class="fas fa-stopwatch-20"></i>
+                        <h4><a href="about.html" class="title-head">Save Time and Money</a></h4>
+                        <p class="">Amus a ligula quam tesque et libero ut justo, ultrices in. Ut eu leo non. Duis
+                            sed et dolor amet.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mt-md-0 mt-4">
+                    <div class="area-box">
+                        <i class="fas fa-comments-dollar"></i>
+                        <h4><a href="about.html" class="title-head">Pay Online in Seconds</a></h4>
+                        <p class="">Amus a ligula quam tesque et libero ut justo, ultrices in. Ut eu leo non. Duis
+                            sed dolor et amet.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mt-lg-0 mt-4">
+                    <div class="area-box">
+                        <i class="fas fa-thumbs-up"></i>
+                        <h4><a href="about.html" class="title-head">Satisfaction Guarantee</a></h4>
+                        <p class="">Amus a ligula quam tesque et libero ut justo, ultrices in. Ut eu leo non. Duis
+                            sed dolor et amet.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="w3l-about-2 py-5">
+        <div class="container py-lg-5 py-md-4 py-2">
+            <div class="row justify-content-between align-items-center pb-lg-5">
+                <div class="col-lg-6 about-2-secs-right mb-lg-0 mb-5">
+                    <div class="image-box inverse position-relative">
+                        <div class="image-box__static">
+                            <img src="assets/images/delicieux-burger-ingredients-frais.jpg" alt="" width="364" height="459">
+                        </div>
+                        <div class="image-box__float">
+                            <img src="assets/images/salade-fruits-renversee-sol-etait-gachis-couleurs-textures-vibrantes-generative-ai.jpg" alt="" width="364" height="459">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 about-2-secs-left ps-lg-5 mt-lg-4 mt-5">
+                    <h5 class="sub-title">More than 25 Years of Experience</h5>
+                    <h3 class="title-style">Deliciousness jumping into the mouth</h3>
+                    <p class="mt-4">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+                        praesentium voluptatum
+                        deleniti similique sunt in culpa qui officia</p>
+                    <ul class="mt-4 list-style-lis">
+                        <li><i class="fas fa-check-circle"></i>100% Customer Satisfaction</li>
+                        <li><i class="fas fa-check-circle"></i>Free Collection & Delivery</li>
+                        <li><i class="fas fa-check-circle"></i>Affordable Prices</li>
+                    </ul>
+                    <a href="about.html" class="btn btn-style mt-5">Discover More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     
+    <div class="w3l-grids-block-5 pb-5 pt-md-2 pt-4">
+        <div class="container pb-lg-5 pb-md-4 pb-2">
+            <div class="title-main text-center mx-auto mb-md-5 mb-4" style="max-width:500px;">
+                <h5 class="sub-title">What We Offer</h5>
+                <h3 class="title-style">Our Recettes</h3>
+            </div>
+            <div class="row text-center justify-content-center">
+            @forelse ($recettes as $recette)
+      
+                <div class="col-lg-4 col-md-6">
+                    <div class="servicecard-single">
+                        <div class="grids5-info position-relative">
+                        
+                            <img src="storage/{{$recette->image}}" alt="{{$recette->image}}" class="" height="400px">
+                        </div>
+                        <div class="content-main-top">
+                            <h4><a href="services.html">{{$recette->name}}</a></h4>
+                            <p>{{$recette->description}}</p>
+                            <a class="btn btn-style mt-4" href="{{ route('showOne', ['id' => $recette->id]) }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+           
+    @empty
+    <div class="col-md-12">
+        <h1>no recettes</h1>
+    </div>
+@endforelse
+</div>
+</div>
+</div>  
+</div>
+
+<section class="w3l-call-to-action-6 py-4">
+    <div class="container py-md-5 py-sm-4 py-4">
+        <div class="d-lg-flex align-items-center justify-content-between">
+            <div class="col-lg-7 left-content-call pe-lg-2">
+                <h3 class="title-big">Quality Service with
+                    Free Collection & Delivery</h3>
+            </div>
+            <div class="col-lg-5 right-content-call mt-lg-0 mt-4">
+                <ul class="buttons d-sm-flex align-items-center justify-content-end">
+                    <li class="phone-sec me-xl-4 me-lg-0 me-sm-4"><i class="fas fa-phone-volume"></i>
+                        <a class="call-style-w3" href="tel:+1(23) 456 789 0000">+1(23) 456 789 0000</a>
+                    </li>
+                    <li><a href="contact.html" class="btn btn-style mt-sm-0 mt-3">Order Now</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+{{--     
 <h1 class="mx-5 mb-5 kilua">LAST RACETTES:</h1>
 
 <div class="row">
@@ -42,10 +157,13 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">{{$recette->name}}</h5>
-                    <img src="storage/{{$recette->image}}" alt="{{$recette->image}}" class="img-fluid w-100 rounded-3">
+                    <img src="/storage/{{$recette->image}}" alt="{{$recette->image}}" class="img-fluid w-100 rounded-3">
                     <p class="card-text">{{$recette->description}}<br></p>
                     <p class="card-text">choumicha</p>
                     <div class="specs"></div>
+                    <div class="card-footer">
+                        <a class="showw " href="{{ route('showOne', ['id' => $recette->id]) }}">Show</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -55,14 +173,74 @@
         </div>
     @endforelse
 </div>
-</section>
+</section> --}}
 
 
 
 
 
 
-
+<footer class="w3l-footer-29-main">
+    <div class="footer-29-w3l py-5">
+        <div class="container pt-md-5 pt-4">
+            <div class="w3l-footer-text-style">
+                {{-- <div class="footer-list-cont d-flex align-items-center justify-content-between mb-5">
+                    <h6 class="foot-sub-title">Contact Us</h6>
+                    <div class="main-social-footer-29">
+                        <a href="#facebook" class="facebook"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#twitter" class="twitter"><i class="fab fa-twitter"></i></a>
+                        <a href="#instagram" class="instagram"><i class="fab fa-instagram"></i></a>
+                        <a href="#linkedin" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div> --}}
+            </div>
+            <div class="row footer-top-29 pt-lg-5 pt-sm-4">
+                <div class="col-lg-3 col-sm-6">
+                    <div class="address-grid">
+                        <h5>10001 Alleghany st, 5th Avenue, 235 Terry, <br> London.</h5>
+                        <h5 class="mt-sm-5 mt-4">Everyday: <span> 7 AM - 8 PM</span></h5>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 mt-sm-0 mt-4">
+                    <div class="address-grid">
+                        <h5 class="top-bold">Phone</h5>
+                        <h5 class="phone-number-text mt-2"><a href="tel:+1(21) 112 7368">+1(21) 112 7368</a></h5>
+                    </div>
+                    <div class="address-grid mt-sm-5 mt-4">
+                        <h5 class="top-bold">E-mail</h5>
+                        <h5 class="email-cont-text mt-1"> <a href="mailto:photogenic@mail.com"
+                                class="mail">photogenic@mail.com</a></h5>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6 col-sm-5 footer-list-menu ps-lg-0 mt-lg-0 mt-sm-5 mt-4">
+                    <div class="address-grid">
+                        <h5 class="mb-sm-4 mb-3 pb-lg-2 top-bold">Support Links</h5>
+                        <ul>
+                            <li><a href="#privacy">Privacy Policy</a></li>
+                            <li><a href="#terms"> Terms of Service</a></li>
+                            <li><a href="contact.html">Contact us</a></li>
+                            <li><a href="#support"> Support</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="address-grid col-lg-4 col-md-6 col-sm-7 mt-lg-0 mt-sm-5 mt-4 w3l-footer-16-main">
+                    <h5 class="top-bold">Subscribe Here</h5>
+                    <form action="#" class="subscribe d-flex mt-sm-4 mt-3 pt-lg-2 mb-4" method="post">
+                        <input type="email" name="email" placeholder="Email Address" required="">
+                        <button><span class="fa fa-paper-plane" aria-hidden="true"></span></button>
+                    </form>
+                    <p>Subscribe to our mailing list and get updates to your email inbox.</p>
+                </div>
+            </div>
+            <!-- copyright -->
+            <div class="w3l-copyright text-center mt-lg-5 mt-4 pt-sm-4 pt-3">
+                <p class="copy-footer-29">© 2022 Laundries. All rights reserved. Design by <a
+                        href="https://w3layouts.com/" target="_blank">
+                        W3layouts</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
